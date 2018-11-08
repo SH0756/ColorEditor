6871451a1d80700b4928a53986652b58b112e01e
SH0756 <shusui000@gmail.com>
2018-11-09 01:23:28 +0900

    無視リストとカラー拡張のベースとなるファイルを追加

---
+++ Assets/ColorEditor/Script/EditColorInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CustomEditor(typeof(EditColorController))]
+public class EditColorInspector : Editor
+{
+    private EditColorController editColorController = null;
+
+    private void OnEnable()
+    {
+        editColorController = (EditColorController)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // TODO : 以下に Inspector 上の GUI を書き込む
+
+        GUILayout.Label("ColorEditorInspector");
+    }
+
+}
