cdae9a0cd0aee67538ed629e947ffe8f7f69401a
SH0756 <shusui000@gmail.com>
2018-11-12 23:08:26 +0900

    ColorEditorWindowのクラスを作成

---
+++ Assets/ColorEditor/Script/ColorEditorWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ColorEditorWindow : EditorWindow
+{
+    /// <summary>
+    /// エディタの作成
+    /// </summary>
+    [MenuItem("Editor/ColorEditor")]
+    public static void Create()
+    {
+        ColorEditorWindow window = GetWindow<ColorEditorWindow>();
+        window.minSize = new Vector2(320, 320);
+    }
+    
+    private void OnEnable()
+    {
+        // TODO : エディタの初期化
+    }
+
+    private void OnGUI()
+    {
+        // TODO : エディタのＵＩ
+    }
+}
